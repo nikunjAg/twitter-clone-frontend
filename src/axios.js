00023d0f4dca12925fe01a5d6ed20f603e3edbcc
nikunjAg <aggarwalnikunj1999@gmail.com>
2021-06-18 09:39:40 +0530

    Preparing for deplyment

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:8080',
+	baseURL: 'https://twitter-server-clone.herokuapp.com',
 });
 
 export default instance;
