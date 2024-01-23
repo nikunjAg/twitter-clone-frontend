3bafb23f22fbd3c9331b4a6f348a546ff8151052
Nikunj Aggarwal <aggarwalnikunj1999@gmail.com>
2024-01-23 20:21:51 +0530

    updated the server url

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const serverBaseURL = 'https://twitter-server-clone.herokuapp.com';
+const serverBaseURL = 'https://twitter-clone-be-bvqx.onrender.com';
 // const serverBaseURL = 'http://localhost:8080';
 
 const instance = axios.create({
